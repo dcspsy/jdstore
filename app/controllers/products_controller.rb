463ec1c067af03926303613d7bb31cb00af86415
pan <dcspsy@gmail.com>
2017-02-09 05:28:16 +0800

    implement product show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,7 @@
-class Admin::ProductsController < ApplicationController
+class ProductsController < ApplicationController
   before_action :authenticate_user!
-  before_action :admin_required
 
-  layout "admin"
+
 
   def index
     @products =Product.all
@@ -15,7 +14,7 @@ class Admin::ProductsController < ApplicationController
   def create
     @product =Product.new(product_params)
     if @product.save
-      redirect_to admin_products_path
+      redirect_to products_path
     else
       render :new
     end
@@ -28,7 +27,7 @@ class Admin::ProductsController < ApplicationController
   def update
     @product = Product.find(params[:id])
     if @product.update(product_params)
-      redirect_to admin_products_path
+      redirect_to products_path
     else
       render :edit
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :products
   end
+
+  resources :products
   root "welcome#index"
 end
